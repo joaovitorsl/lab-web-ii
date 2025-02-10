56cea2f1a7869dc53ce2426813243703e17de24b
João Vitor <joao.luciano@lsd.ufcg.edu.br>
2025-02-10 19:39:01 -0300

    upload lab

---
+++ lab-02/src/models/User.ts
@@ -0,0 +1,46 @@
+import { Model, DataTypes } from "sequelize";
+import sequelize from "../config/database";
+
+// Defina os atributos do modelo
+interface UserAttributes {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+}
+
+export class User extends Model<UserAttributes> implements UserAttributes {
+  public id!: number;
+  public name!: string;
+  public email!: string;
+  public password!: string;
+}
+
+// Inicialize o modelo com os campos no banco
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: "users",
+    timestamps: false,
+  }
+);
